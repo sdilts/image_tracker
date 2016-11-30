3c1f479a2325355a6828851b4b56d91fadbea59d
Alex Irish <irish3725@gmail.com>
2016-11-30 12:40:58 -0700

    Added test file and implemented suggested changes

--- src/main/java/csci432/SigmaDeltaFilter.java
+++ src/main/java/csci432/SigmaDeltaFilter.java
@@ -1,19 +1,73 @@
 package csci432;
 
 import java.awt.image.BufferedImage;
-import java.util.ArrayList;
-import java.util.List;
 
 public class SigmaDeltaFilter {
 
-    List<BufferedImage> imageList;
+    private int numFiltered;
+    private BufferedImage background;
 
+    /**
+     * Initializes SigmaDelta Filter
+     **/
     public SigmaDeltaFilter() {
-        this.imageList = new ArrayList<BufferedImage>();
+        this.numFiltered = 0;
     }
 
-    public BufferedImage Filter(BufferedImage b) {
-        return b;
+    /**
+     * Decides either to filter image, or just set the background using
+     * a threshold for images processed so far.
+     *
+     * @param image a BufferedImage to be filtered
+     *
+     * @return the newly filtered image if numFiltered is more than
+     * 20 or just the original image if numFiltered is 20 or less
+     **/
+    public BufferedImage filter(BufferedImage image) {
+        if (numFiltered <= 20) {
+            setBackground(image);
+        } else {
+            image = filterImage(image);
+        }
+
+        return image;
     }
 
+    /**
+     * Uses image to continue the averaging of pixels in background
+     *
+     * @param image a BufferedImage to be filtered
+     **/
+    public void refreshBackground(BufferedImage image) {
+        //pixel averaging loop
+    }
+
+    /**
+     * Starts to average pixels in background. Threshold for
+     * this can be changed. If this is the first image,
+     * background is initialized as that image.
+     *
+     * @param image a BufferedImage to be filtered
+     **/
+    public void setBackground(BufferedImage image) {
+        if (numFiltered == 0) {
+            background = image;
+        } else {
+            refreshBackground(image);
+        }
+    }
+
+    /**
+     * Calls refreshBackground() then removes background from current image
+     * and returns new backgroundless image
+     *
+     * @param image a BufferedImage to be filtered
+     *
+     * @return the newly filtered image
+     **/
+    public BufferedImage filterImage(BufferedImage image) {
+        refreshBackground(image);
+        //pixel removal loop
+        return image;
+    }
 }
---
+++ src/test/java/csci432/SigmaDeltaFilterTest.java
@@ -0,0 +1,25 @@
+package java.csci432;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by alex on 11/30/16.
+ */
+public class SigmaDeltaFilterTest extends TestCase {
+    public void testFilter() throws Exception {
+
+    }
+
+    public void testRefreshBackground() throws Exception {
+
+    }
+
+    public void testSetBackground() throws Exception {
+
+    }
+
+    public void testFilterImage() throws Exception {
+
+    }
+
+}

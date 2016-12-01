2c5e5dfb6331046aa63cef5383fa5ddc31655725
Sage.Smith <sage.smith@workiva.com>
2016-12-01 15:21:37 -0700

    Added options and tests

--- src/test/java/csci432/MainTest.java
+++ src/test/java/csci432/MainTest.java
@@ -1,5 +1,6 @@
 package csci432;
 
+import joptsimple.OptionSet;
 import org.junit.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -8,7 +9,13 @@ import static org.hamcrest.Matchers.is;
 
 public class MainTest {
     @Test
-    public void helloWorldTest() {
-        assertThat(Main.helloWorld(), is(equalTo("Hello World!")));
+    public void getOptionsTest() {
+        OptionSet optionSet = Main.getOptions("-save_loc", "sage is cool");
+        assertThat(optionSet.valueOf("save_loc").toString(), is(equalTo("sage is cool")));
+    }
+
+    @Test(expected = Exception.class)
+    public void getOptionsIncorrect() {
+        Main.getOptions("-f", "steve");
     }
 }

ac138739f8042b8dafe693c661543ac72fc5d299
irish3725 <irish3725@gmail.com>
2016-12-01 18:10:33 -0700

    Filled completed SIgmaDeltaFilter class.
    The class now has a main filter() method that decides whether to initialize the background or actually filter the image. I added two BufferedImage variables that hold the different implimentations of the filter. The subtraction is probably the most effective one though. There is also an integer called bBuffer that holds the amount that we want to buffer the background averaging and an integer that holds the number of pictures we want to use to initialize the background. The refreshBackground() method just averages each pixel in the background with the current image. If bBuffer is changed, it will change how this averaging is done. filterImageSubtract() is a method that subtracts the RGB values of each of the pixels in the background from the corresponding RGB  values in the inputted image to remove the background. filterImageRemove writes a 0 to the RGB values in the inputted image that match the RGB values of the corresponding pixels in the background. This method could be changed a little to give us darker blobs, but I think filterImageSubtract() will work better.

--- src/test/java/csci432/SigmaDeltaFilterTest.java
+++ src/test/java/csci432/SigmaDeltaFilterTest.java
@@ -10,7 +10,7 @@ import org.junit.Test;
 import java.awt.image.BufferedImage;
 
 /**
- * Created by alex on 11/30/16.
+ * Created by alex on 12/1/16.
  */
 public class SigmaDeltaFilterTest  {
     SigmaDeltaFilter f;
@@ -21,33 +21,94 @@ public class SigmaDeltaFilterTest  {
     }
 
     @Test
-    public void testRefreshBackground()  {
-        f.refreshBackground(new BufferedImage(300, 300, BufferedImage.TYPE_BYTE_BINARY));
-    }
-
-    @Test
-    public void testSetBackground() {
-        BufferedImage a = new BufferedImage(300, 300, BufferedImage.TYPE_3BYTE_BGR);
-        f.setBackground(a);
-        f.filter(a);
-        f.setBackground(a);
-    }
-
-    @Test
     public void testFilter() {
         BufferedImage a = new BufferedImage(300, 300, BufferedImage.TYPE_3BYTE_BGR);
-        BufferedImage b = f.filter(a);
-        assertThat(b,is(equalTo(a)));
+        BufferedImage b = a;    //will be calculated background
+        BufferedImage c = a;    //save result here
+        BufferedImage s = a;    //will be calculated result
 
-        for(int i = 0; i < 22; i++){
-            b = f.filter(a);
+        for(int i = 0; i < a.getHeight(); i++) {
+            for(int j = 0; j < a.getWidth(); j++) {
+                a.setRGB(j, i, 10);
+                b.setRGB(j, i, 10);
+            }
         }
-        assertThat(b,is(equalTo(a)));
+        c = f.filter(a);
+        assertThat(f.background, equalTo(b));
+        assertThat(c,is(equalTo(a)));
+
+        for(int i = 1; i < f.initBackground ; i++){
+            int aPixel = a.getRGB(0,0) + i + 1;
+            int bPixel = b.getRGB(0, 0) + i;
+            for(int y = 0; y < a.getHeight(); y++) {
+                for(int x = 0; x < a.getWidth(); x++) {
+                    a.setRGB(x, y, aPixel);
+                    b.setRGB(x, y, bPixel);
+                    s.setRGB(x, y, aPixel - bPixel);
+                }
+            }
+            c = f.filter(a);
+            assertThat(b, equalTo(f.background));
+            assertThat(c, equalTo(a));
+        }
+
+        int i = f.initBackground;
+        int aPixel = a.getRGB(0,0) + i + 1;
+        int bPixel = b.getRGB(0, 0) + i;
+        for(int y = 0; y < a.getHeight(); y++) {
+            for(int x = 0; x < a.getWidth(); x++) {
+                a.setRGB(x, y, aPixel);
+                b.setRGB(x, y, bPixel);
+            }
+        }
+        c = f.filter(a);
+        assertThat(b, equalTo(f.background));
+        assertThat(c,is(equalTo(s)));
     }
 
     @Test
-    public void testFilterImage() {
-        f.filterImage(new BufferedImage(300, 300, BufferedImage.TYPE_BYTE_BINARY));
+    public void testRefreshBackground()  {
+        BufferedImage a = new BufferedImage(300, 300, BufferedImage.TYPE_BYTE_GRAY);
+        BufferedImage b = a;
+        BufferedImage c = a;
+        for (int i = 0; i < a.getHeight(); i++){
+            for (int j = 0; j < a.getWidth(); j++){
+                a.setRGB(j, i, 4);
+                b.setRGB(j, i, 8);
+                c.setRGB(j, i, 6);
+            }
+        }
+        f.background = a;
+        f.refreshBackground(b);
+        assertThat(f.background, equalTo(c));
     }
+
+    @Test
+    public void testFilterImageSubtract() {
+        BufferedImage a = new BufferedImage(300, 300, BufferedImage.TYPE_BYTE_GRAY);
+        BufferedImage b = a;
+        BufferedImage c = a;
+        BufferedImage d = a;
+        for (int i = 0; i < a.getHeight(); i++){
+            for (int j = 0; j < a.getWidth(); j++){
+                a.setRGB(j, i, 10);
+                b.setRGB(j, i, 8);
+                c.setRGB(j, i, 2);
+            }
+        }
+        //check to see if it works subtracting lower from higher
+        f.background = a;
+        d = f.filterImageSubtract(b);
+        assertThat(d, equalTo(c));
+
+        //check to see if it works subtracting higher from lower
+        f.background = b;
+        d = f.filterImageSubtract(a);
+        assertThat(d, equalTo(c));
+    }
+
+
+
+
 
 }
